bade19fdeffccf9e88c3cf9850bb6e1737d3855a
popopocorn <popcorn375@naver.com>
2025-03-19 15:33:35 +0900

    Update CGameFramework.h

--- LabProjcets02/CGameFramework.h
+++ LabProjcets02/CGameFramework.h
@@ -9,7 +9,20 @@ public:
 	IDXGISwapChain3* m_pdxgiSwapChain;
 	ID3D12Device* m_pd3dDevice;
 
-	bool m_bM;
+	bool m_bMsaa4xEnable = false;
+
+	UINT m_nMsaa4xQualityLevels = 0;
+
+	static const UINT m_nSwapChainBuffers = 2;
+	UINT m_nSwapChainBufferIndex;
+
+	ID3D12Resource* m_ppd3dRenderTargetBuffers[m_nSwapChainBuffers];
+	ID3D12DescriptorHeap* m_pd3dRtvDescriptorHeap;
+	UINT m_nRtvDescriptorIncrementSize;
+
+	ID3D12Resource* m_pd3dDepthStencilBuffer;
+	ID3D12DescriptorHeap* m_pd3dDsvDescriptorHeap;
+	UINT m_nDsvDescriptorIncrementSize;
 
 };
 
