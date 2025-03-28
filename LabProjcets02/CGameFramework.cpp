47175e5a9f7f4895b6262efd4ac1b5c57e61a1c3
popopocorn <popcorn375@naver.com>
2025-03-29 02:05:24 +0900

    프레임 워크 제작
    따라하기 03 CGameFramework.h 완료
    CGameFramework.cpp OnDestroy까지 완료

--- LabProjcets02/CGameFramework.cpp
+++ LabProjcets02/CGameFramework.cpp
@@ -3,10 +3,94 @@
 
 CGameFramework::CGameFramework()
 {
+	m_pdxgiFactory = NULL;
+	m_pdxgiSwapChain = NULL;
+	m_pd3dDevice = NULL;
+
+	m_pd3dCommandAllocator = NULL;
+	m_pd3dCommandQueue = NULL;
+	m_pd3dpipelinedState = NULL;
+	m_pd3dCommandList = NULL;
+
+	for (int i = 0; i < m_nSwapChainBuffers; ++i) 
+		m_ppd3dRenderTargetBuffers[i] = NULL;
+	m_pd3dRtvDescriptorHeap = NULL;
+	m_nRtvDescriptorIncrementSize = 0;
+
+	m_pd3dDepthStencilBuffer = NULL;
+	m_pd3dDsvDescriptorHeap = NULL;
+	m_nDsvDescriptorIncrementSize = 0;
+
+	m_nSwapChainBufferIndex = 0;
+
+	m_hFenceEvent = NULL;
+	m_pd3dFence = NULL;
+	m_nFenceValue = 0;
+
+	m_nWndClientWidth= FRAME_BUFFER_WIDTH;
+	m_nWndClientHeight = FRAME_BUFFER_HEIGHT;
+
 
 }
 
 CGameFramework::~CGameFramework()
 {
+
+}
+
+
+bool CGameFramework::OnCreate(HINSTANCE hInstance, HWND hMainWnd) {
+	m_hInstance = hInstance;
+	m_hWnd = hMainWnd;
+
+	CreateDirect3DDevice();
+	CreateCommandQueueAndList();
+	CreateSwapChain();
+	CreateRtvAndDsvDescriptorHeaps();
+	CreateRenderTargetViews();
+	CreateDepthStencilView();
+
+	BuildObjects();
+
+	return(true);
+
+}
+
+void CGameFramework::OnDestroy() {
+	WaitForGpuComplete();
+
+	ReleaseObjects();
+
+	::CloseHandle(m_hFenceEvent);
+
+	for (int i = 0; i < m_nSwapChainBuffers; ++i) {
+		if (m_ppd3dRenderTargetBuffers[i]) {
+			m_ppd3dRenderTargetBuffers[i]->Release();
+		}
+	}
+
+	if (m_pd3dRtvDescriptorHeap) m_pd3dRtvDescriptorHeap->Release();
+	if (m_pd3dDsvDescriptorHeap) m_pd3dDsvDescriptorHeap->Release();
+	
+	if (m_pd3dCommandAllocator) m_pd3dCommandAllocator->Release();
+	if (m_pd3dCommandQueue) m_pd3dCommandQueue->Release();
+	if (m_pd3dpipelinedState) m_pd3dpipelinedState->Release();
+	if (m_pd3dCommandList) m_pd3dCommandList->Release();
+
+	if (m_pd3dFence) m_pd3dFence->Release();
+
+	m_pdxgiSwapChain->SetFullscreenState(FALSE, NULL);
+	if (m_pdxgiSwapChain) m_pdxgiSwapChain->Release();
+	if (m_pd3dDevice) m_pd3dDevice->Release();
+	if (m_pdxgiFactory) m_pdxgiFactory->Release();
+
+#if defined(_DEBUG)
+	IDXGIDebug1* pdxgiDebug = NULL;
+	DXGIGetDebugInterface1(0, __uuidof(IDXGIDebug1), (void**)&pdxgiDebug);
+	HRESULT hResult = pdxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_DETAIL);
+	pdxgiDebug->Release();
+
+
+#endif
 
 }
